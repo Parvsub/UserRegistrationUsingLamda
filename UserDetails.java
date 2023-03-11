7b09987df840b639dd131c5669d020298168abbb
Parvsub <Praveenboda421@gmail.com>
2023-03-11 14:05:09 +0530

    Added: LastName is validated

--- UserDetails.java
+++ UserDetails.java
@@ -2,9 +2,15 @@ package com.UserRegistrationUsinglamda;
 
 public class UserDetails {
     String firstName;
+    String LastName;
 
-    public UserDetails(String firstName){
+    public UserDetails(String firstName,String LastName){
         this.firstName=firstName;
+        this.LastName = LastName;
+
     }
     public UserDetails(){}
+
+    public UserDetails(String nextLine) {
+    }
 }
--- UserRegistrationValidation.java
+++ UserRegistrationValidation.java
@@ -5,21 +5,31 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-    public class UserRegistrationValidation {
+public class UserRegistrationValidation {
 
-        public void getFirstName() {
-            UserRegistrationValidation user = new UserRegistrationValidation();
-            Scanner scanner = new Scanner(System.in);
-            System.out.println("Enter the first name");
-            UserDetails userDetails = new UserDetails(scanner.nextLine());
-            Pattern pattern = Pattern.compile("([A-Z]{1,}[a-z]{3,})");
-            Matcher matcher = pattern.matcher(userDetails.firstName);
-            boolean m = matcher.matches();
-            System.out.println(m);
-        }
-        public static void main(String[] args) {
-            UserRegistrationValidation user=new UserRegistrationValidation();
-            user.getFirstName();
-        }
+    public void getFirstName() {
+        UserRegistrationValidation user = new UserRegistrationValidation();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the first name");
+        UserDetails userDetails = new UserDetails(scanner.nextLine());
+        Pattern pattern = Pattern.compile("([A-Z]{1,}[a-z]{3,})");
+        Matcher matcher = pattern.matcher(userDetails.firstName);
+        boolean m = matcher.matches();
+        System.out.println(m);
     }
-
+    public void getLastName() {
+        UserRegistrationValidation user = new UserRegistrationValidation();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the Last name");
+        UserDetails userDetails = new UserDetails(scanner.nextLine());
+        Pattern pattern = Pattern.compile("([A-Z][a-z]{2,})");
+        Matcher matcher = pattern.matcher(userDetails.LastName);
+        boolean m = matcher.matches();
+        System.out.println(m);
+    }
+    public static void main(String[] args) {
+        UserRegistrationValidation user=new UserRegistrationValidation();
+        user.getFirstName();
+        user.getLastName();
+    }
+}
